04551f0f6230eb5c140caa625a71fa76173aa0bd
James Haff <jehaff@James-Haffs-MacBook-Pro.local>
2015-08-18 14:46:19 -0500

    backbone boilerPlate

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'bundler'
+Bundler.require
+
+  set :views, File.expand_path('../views', __FILE__)
+  set :public, File.expand_path('../public', __FILE__)
+
+
+get '/' do
+
+  erb :index
+
+end
